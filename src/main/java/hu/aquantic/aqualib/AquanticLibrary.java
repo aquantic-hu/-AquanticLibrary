354c305af3b0c144e58cf2c478e2a31f60be3856
Szabolcs <github.com@nerman.hu>
2023-08-05 20:44:41 +0200

    Új funkció
     • Egyedi szöveget feldolgozó függvény

--- src/main/java/hu/aquantic/aqualib/AquanticLibrary.java
+++ src/main/java/hu/aquantic/aqualib/AquanticLibrary.java
@@ -3,13 +3,12 @@ package hu.aquantic.aqualib;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import static hu.aquantic.aqualib.Utils.tell;
+import static hu.aquantic.aqualib.Utils.AquaLog;
 
 public final class AquanticLibrary extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        tell(Bukkit.getConsoleSender(), "[" + LibComplex.getPlugin().getName() + "] " + "&aAquanticLibrary betöltve!");
 
     }
 
--- src/main/java/hu/aquantic/aqualib/Utils.java
+++ src/main/java/hu/aquantic/aqualib/Utils.java
@@ -3,15 +3,27 @@ package hu.aquantic.aqualib;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
 
 public class Utils {
-    public static void tell(final CommandSender sender, final String message) {
-        sender.sendMessage(colorize(message));
+    public static void AquaLog(Integer level, String message) {
+        switch (level) {
+            case 0 -> Bukkit.getLogger().log(Level.INFO,    ChatColor.translateAlternateColorCodes('&', message));
+            case 1 -> Bukkit.getLogger().log(Level.WARNING, ChatColor.translateAlternateColorCodes('&', message));
+            case 2 -> Bukkit.getLogger().log(Level.SEVERE,  ChatColor.translateAlternateColorCodes('&', message));
+            default -> throw new IllegalStateException("Nincs megadva a logolási fokozat! -> " + level);
+        }
     }
-    public static void log(final String message) {
-        tell(Bukkit.getConsoleSender(), "[" + LibComplex.getPlugin().getName() + "] " + message);
-    }
-    public static String colorize(final String message) {
-        return ChatColor.translateAlternateColorCodes('&', message);
+    public static void AquaText(Integer type, String message, CommandSender sender) {
+        Player p = (Player) sender;
+        switch (type) {
+            case 1 -> p.sendMessage(message);
+            case 2 -> Bukkit.broadcastMessage(message);
+            case 3 -> p.sendPlainMessage(message);
+            default -> throw new IllegalStateException("Hibásan lett megadva az 'AquaText' függvény! -> " + type);
+        }
     }
 }
