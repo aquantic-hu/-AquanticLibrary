569cfea8985e6de872c234576945def8c79e20d1
Szabolcs <github.com@nerman.hu>
2023-08-07 07:28:00 +0200

    Módosítások
     • A 'Utils' osztály mostantól 'ChatUtils' nevű
     • Verzió javítva a pom file-ban: v1.1.2-ALPHA

--- src/main/java/hu/aquantic/aqualib/ChatUtils.java
+++ src/main/java/hu/aquantic/aqualib/ChatUtils.java
@@ -8,7 +8,7 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.logging.Level;
 
-public class Utils {
+public class ChatUtils {
 
     public static void AquaLog(@NotNull Integer level, String message) {
         switch (level) {
